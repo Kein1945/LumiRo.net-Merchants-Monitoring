a71eb28ff56e3d2d5de569cef1dafd286e423bbd
Kein <kein.1945@gmail.com>
2012-10-04 18:00:28 +0400

    Background page fix

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 $(function(){
-  (function timed(){
-    setTimeout(arguments.callee, 20*20000);
-    Merchants.eachMerch(function(merch){
-      merch.update();
-    });
-  })();
+    (function timed(){
+        setTimeout(arguments.callee, 20*20000);
+        Merchants.eachMerch(function(merch){
+            merch.update();
+        });
+    })();
 });
