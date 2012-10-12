47278ecb8c2f0ddaecde1a3bdd8a8035cec3ceb2
Kein <kein.1945@gmail.com>
2012-10-12 16:45:17 +0400

    Extension icon badge text update fix

--- js/background.js
+++ js/background.js
@@ -1,25 +1,21 @@
 $(function(){
     (function(){
-        setTimeout(arguments.callee, 20*20000);
-        var callback_wait = 0
-            , calc_profit = function(){
-                callback_wait--
-                if(callback_wait < 1) {
-                    var profit = _.reduce(Tracker.listMerchants(), function(profit, merch){
-                        return profit += merch.data.profit.summary;
-                    }, 0)
-                    if(profit > 100000)
-                        chrome.browserAction.setBadgeText({text:
-                            (profit > 100000)?String( (profit/1000000).toFixed(1)):''
-                        })
-                }
-            }
-        calc_profit()
+        setTimeout(arguments.callee, 5*60000);
         _.each(Tracker.listMerchants(), function(merch){
-            callback_wait++
-            merch.update(function(){
-
-            });
+            merch.update();
         });
+    })();
+
+    (function(){
+        var profit = _.reduce(Tracker.listMerchants(), function(profit, merch){
+            return profit += merch.data.profit.summary;
+        }, 0)
+        if(profit > 100000)
+            chrome.browserAction.setBadgeText({text:
+                (profit > 100000)?String( (profit/1000000).toFixed(1)):''
+            })
+        else
+            chrome.browserAction.setBadgeText({text:""})
+        setTimeout(arguments.callee, 30000);
     })();
 });
