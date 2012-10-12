f18f4ed094173c5467cc2ab1e4358cffded685f2
-Kein- <kein.1945@gmail.com>
2012-10-13 03:48:55 +0400

    Compare products function fix

--- js/lumi/tracker.js
+++ js/lumi/tracker.js
@@ -45,13 +45,17 @@ var Merchant = function(name){
         call_waiting ++
         market.load.sell(self.data.name, function(new_items){
             //self.proceedSellData(new_items)
-            sell_result = self.compareLists(self.data.items.sell, new_items);
+            sell_result = self.compareLists(self.data.items.sell, new_items, function(item){
+                return 'hash' + item.id + "|" + item.refine + ':' + item.price + '+' + item.slots + '/' + _.reduce(item.features,function(memo, feature){ return '|' + features.id },'|');
+            });
             callback_func()
         });
         call_waiting ++
         market.load.buy(self.data.name, function(new_items){
             //self.proceedBuyData(new_items)
-            buy_result = self.compareLists(self.data.items.buy, new_items);
+            buy_result = self.compareLists(self.data.items.buy, new_items, function(item){
+                return 'hash' + item.id + ':' + item.price;
+            });
             callback_func()
         });
     };
@@ -73,162 +77,6 @@ Merchant.prototype.calcProfit = function(){
         profit += (item.count - item.amount)* item.price;
     }
     this.data.profit.summary = profit;
-}
-
-Merchant.prototype.proceedSellData = function(new_items){
-    var items = []
-        , profit = 0
-        , old_items = this.data.items.sell
-        , flag = {isReset: false, isNewVend: false, isFall: false}
-        , getItemHash = function(item){
-            return 'hash' + item.id + "|" + item.refine + ':' + item.price + '+' + item.slots + '/' + _.reduce(item.features,function(memo, feature){ return '|' + features.id },'|');
-        };
-    if(0 < old_items.length){
-        if(0 < new_items.length){ // Предметы есть
-
-            var old_item_checked = new Array(); // Предметы проверенные
-            var new_item_checked = new Array(); // Предметы проверенные
-            // Ключ каждого массива это хэш предмета и его номер.
-            // Блок поиска предметов которые у есть как в старом так и в новом списке
-            for(var i= 0; i<old_items.length; i++){
-                var old_item_hash = getItemHash(old_items[i])
-                for(var j= 0; j<new_items.length; j++){
-                    var new_item_hash = getItemHash(new_items[j])
-                    if(!old_item_checked[old_item_hash+i] // Мы не проверяли предмет
-                        && !new_item_checked[new_item_hash+j] // Мы не проверяли предмет
-                        // Хэши предметов совпадают - наверное один и тот же
-                        && new_item_hash == old_item_hash
-                        && old_items[i].count >= new_items[j].count){
-                        old_item_checked[old_item_hash+i] = true;
-                        new_item_checked[old_item_hash+j] = true;
-                        old_items[i].amount = new_items[j].count;
-                    }
-                }
-            }
-            // Проверям проданные вещи, которых не оказалось в новом списке
-            for(var i=0; i<old_items.length; i++){
-                old_item_hash = getItemHash(old_items[i]);
-                if(!old_item_checked[old_item_hash+i]){
-                    old_items[i].amount = 0; // значит их осталось 0 штук ;)
-                }
-            }
-            items = old_items;
-
-            // Подсчитаем прибыль
-            for(var i=0; i<items.length; i++){
-                var item = items[i];
-                profit += (item.count - item.amount) * item.price
-            }
-
-            // Проверяем наличие новых предметов которых нет в старом венде
-            for(var j=0; j<new_items.length; j++){
-                var new_item_hash = getItemHash(new_items[j]);
-                for(var i=0; i<old_items.length; i++){
-                    var found = false
-                        , old_item_hash = getItemHash(old_items[i]);
-                    if(new_item_hash == old_item_hash){
-                        found = true; break;
-                    }
-                }
-                if(!found){
-                    flag.isNewVend = true
-                    //this.data.profit.summary = 0
-                    items = new_items
-                    notify(this.data.name, 'Новый венд. Данные торговца обновленны.');
-                    break;
-                }
-            }
-
-        } else { // Наш венд слетел
-            items = []
-            flag.isFall = true;
-        }
-    } else {// Если раньше у нас не было новых вещей
-        flag.isNewVend = !!(items = new_items).length;
-    }
-    this.data.items.sell = items;
-    this.data.profit.last = profit - this.data.profit.summary;
-    this.data.profit.summary = profit;
-    return flag;
-    // profit > 0 && notify(self.data.name, '+ '+number_format(profit, {decimals: 0, thousands_sep: "."}));
-}
-
-
-Merchant.prototype.proceedBuyData = function(new_items){
-    var items = []
-        , profit = 0
-        , old_items = this.data.items.buy
-        , flag = {isReset: false, isNewVend: false, isFall: false}
-        , getItemHash = function(item){
-            return 'hash' + item.id + ':' + item.price;
-        };
-    if(0 < old_items.length){
-        if(0 < new_items.length){ // Предметы есть
-
-            var old_item_checked = new Array(); // Предметы проверенные
-            var new_item_checked = new Array(); // Предметы проверенные
-            // Ключ каждого массива это хэш предмета и его номер.
-            // Блок поиска предметов которые у есть как в старом так и в новом списке
-            for(var i= 0; i<old_items.length; i++){
-                var old_item_hash = getItemHash(old_items[i])
-                for(var j= 0; j<new_items.length; j++){
-                    var new_item_hash = getItemHash(new_items[j])
-                    if(!old_item_checked[old_item_hash+i] // Мы не проверяли предмет
-                        && !new_item_checked[new_item_hash+j] // Мы не проверяли предмет
-                        // Хэши предметов совпадают - наверное один и тот же
-                        && new_item_hash == old_item_hash
-                        && old_items[i].amount >= new_items[j].count){
-                        old_item_checked[old_item_hash+i] = true;
-                        new_item_checked[old_item_hash+j] = true;
-                        old_items[i].amount = new_items[j].count;
-                    }
-                }
-            }
-            // Проверям проданные вещи, которых не оказалось в новом списке
-            for(var i=0; i<old_items.length; i++){
-                old_item_hash = getItemHash(old_items[i]);
-                if(!old_item_checked[old_item_hash+i]){
-                    old_items[i].amount = 0; // значит их осталось 0 штук ;)
-                }
-            }
-            items = old_items;
-
-            // Подсчитаем прибыль
-            for(var i=0; i<items.length; i++){
-                var item = items[i];
-                profit += (item.count - item.amount) * item.price
-            }
-
-            // Проверяем наличие новых предметов которых нет в старом венде
-            for(var j=0; j<new_items.length; j++){
-                var new_item_hash = getItemHash(new_items[j]);
-                for(var i=0; i<old_items.length; i++){
-                    var found = false
-                        , old_item_hash = getItemHash(old_items[i]);
-                    if(new_item_hash == old_item_hash){
-                        found = true; break;
-                    }
-                }
-                if(!found){
-                    flag.isNewVend = true
-                    //this.data.profit.summary = 0
-                    items = new_items
-                    notify(this.data.name, 'Новый венд. Данные торговца обновленны.');
-                    break;
-                }
-            }
-
-        } else { // Наш венд слетел
-            items = []
-            flag.isFall = true;
-        }
-    } else {// Если раньше у нас не было новых вещей
-        flag.isNewVend = !!(items = new_items).length;
-    }
-    this.data.items.buy = items;
-    this.data.lose.last = profit - this.data.lose.summary;
-    this.data.lose.summary = profit;
-    return flag;
 }
 
 /**
